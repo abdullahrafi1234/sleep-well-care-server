4ee080644a202c3734a53fb10b2a355697061c8b
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-04-29 18:56:02 +0600

    delete operation done

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
     })
 
 
+    app.delete('/addTouristsSpot/:id', async(req, res) => {
+        const id = req.params.id
+        const query = {_id : new ObjectId(id)}
+        const result = await touristCollection.deleteOne(query)
+        res.send(result)
+    })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
