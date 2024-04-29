d478e314633a418aa75d87ff233d2e0d75fa7473
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-04-29 19:40:30 +0600

    added update functionality

--- index.js
+++ index.js
@@ -16,67 +16,76 @@ console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const touristCollection = client.db('touristDB').collection('tourist')
-
-
-    app.get('/addTouristsSpot', async(req,res) => {
-        const cursor = touristCollection.find()
-        const result = await cursor.toArray();
-        res.send(result)
-    })
-
-    // view details
-    app.get('/addTouristsSpot/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await touristCollection.findOne(query)
-        res.send(result)
-    })
-
-    // my list
-    app.get('/myList/:email', async(req, res) => {
-        console.log(req.params.email);
-        const result = await touristCollection.find({email:req.params.email}).toArray()
-        res.send(result);
-    })
+        const touristCollection = client.db('touristDB').collection('tourist')
 
 
-    app.post('/addTouristsSpot', async(req, res)=>{
-        const newSpot = req.body
-        console.log(newSpot)
-        const result = await touristCollection.insertOne(newSpot)
-        res.send(result)
-    })
+        app.get('/addTouristsSpot', async (req, res) => {
+            const cursor = touristCollection.find()
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        // view details
+        app.get('/addTouristsSpot/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await touristCollection.findOne(query)
+            res.send(result)
+        })
+
+        // my list
+        app.get('/myList/:email', async (req, res) => {
+            console.log(req.params.email);
+            const result = await touristCollection.find({ email: req.params.email }).toArray()
+            res.send(result);
+        })
 
 
-    app.delete('/addTouristsSpot/:id', async(req, res) => {
-        const id = req.params.id
-        const query = {_id : new ObjectId(id)}
-        const result = await touristCollection.deleteOne(query)
-        res.send(result)
-    })
+        // update
+        app.get('/addTouristsSpot/:id', async(req,res)=> {
+            const id = req.params.id;
+            const query = {_id: new ObjectId()}
+            const result = await touristCollection.findOne(query)
+            res.send(result);
+        })
+
+
+        app.post('/addTouristsSpot', async (req, res) => {
+            const newSpot = req.body
+            console.log(newSpot)
+            const result = await touristCollection.insertOne(newSpot)
+            res.send(result)
+        })
+
+
+        app.delete('/addTouristsSpot/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await touristCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
